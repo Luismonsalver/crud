664dd59a3b161bdb3387ffd2b3ec43928bd058d9
Luismonsalver <126512492+Luismonsalver@users.noreply.github.com>
2023-07-16 03:19:16 -0400

    Modificar jugador agregado

--- script.js
+++ script.js
@@ -47,15 +47,16 @@ const playerRead = () => {
                     <p>Apellido: ${player.LastName}</p>
                     <p>Dorsal: ${player.Number}</p>
                     <p>Posición: ${player.Position}</p>
-
-                    <button data-id="${index}" onclick="deletePlayer(event)">Borrar</button>
+                </div>
+                <button data-id="${index}" onclick="updatePlayer(event)">Modificar</button>
+                <button data-id="${index}" onclick="deletePlayer(event)">Borrar</button>
+                <div id="update">
+                
                 </div>
             `
         });
     }
 }
-
-document.addEventListener("DOMContentLoaded", playerRead)
 
 const deletePlayer = (event) => {
     let index = event.target.getAttribute("data-id");
@@ -69,4 +70,41 @@ const deletePlayer = (event) => {
 
     playerCreate();
 }
+
+const updatePlayer = (event) => {
+    const index = event.target.getAttribute("data-id");
+    const playerToUpdate = arrayPlayers[index];
+
+    const updateDiv = document.querySelector("#update");
+
+    updateDiv.innerHTML= `
+        <input type="text" id="updateName" value = ${playerToUpdate.Name}>
+        <input type="text" id="updateLastname" value = ${playerToUpdate.LastName}>
+        <input type="number" id="updateNumber" min="1" max="99" pattern="[1-9]{1,2}|99" value = ${playerToUpdate.Number}>
+        <button id="confirmButton">Confirmar</button>
+    `
+
+    const updateName = document.querySelector("#updateName");
+    const updateLastname = document.querySelector("#updateLastname");
+    const updateNumber = document.querySelector("#updateNumber");
+
+    const confirmButton = document.querySelector("#confirmButton");
+    confirmButton.addEventListener("click", () => {
+        // Actualizar los valores del jugador con los nuevos valores ingresados
+        playerToUpdate.Name = updateName.value;
+        playerToUpdate.LastName = updateLastname.value;
+        playerToUpdate.Number = updateNumber.value;
+
+        // Volver a guardar los datos actualizados en LocalStorage
+        localStorage.setItem("squad", JSON.stringify(arrayPlayers));
+
+        // Limpiar el div de actualización y volver a mostrar la lista de jugadores
+        updateDiv.innerHTML = "";
+        playerRead();
+    });
+}
+
+document.addEventListener("DOMContentLoaded", playerRead)
+
+
   
